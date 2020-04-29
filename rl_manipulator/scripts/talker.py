dfad31f569202406d059292112be11c4c261a787
robotemperor <kimpd1224@gmail.com>
2020-04-29 10:46:44 +0200

    ros communication added

--- rl_manipulator/scripts/talker.py
+++ rl_manipulator/scripts/talker.py
@@ -1,56 +1,129 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
 
 import rospy
+import tf as tf_data
 from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+import tensorflow as tf
+
+
+from tf_agents.environments import py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import utils
+from tf_agents.specs import array_spec
+from tf_agents.environments import wrappers
+from tf_agents.environments import suite_gym
+from tf_agents.trajectories import time_step as ts
+
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps = 1000  # @param {type:"integer"}
+collect_steps_per_iteration = 1  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 32  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+class optimalPathEnv(py_environment.PyEnvironment):
+  def __init__(self):
+    self._action_spec = array_spec.BoundedArraySpec(
+        shape=(6,), dtype=np.float32, minimum=[0,0,0,0,0,0], maximum=[1,1,1,1,1,1], name='action')
+    self._observation_spec = array_spec.BoundedArraySpec(
+        shape=(6,), dtype=np.float32, name='observation', minimum=[0,0,0,0,0,0], maximum = [1,1,1,1,1,1])
+    self._state = np.array([0,0,0,0,0,0])
+    self._episode_ended = False
+    self._cur_state = np.array([0,0,0,0,0,0])
+    self._desired_state = np.array([0, 0, 0, 0, 0, 0])
+
+  def action_spec(self):
+    return self._action_spec
+
+  def observation_spec(self):
+    return self._observation_spec
+
+  def _reset(self):
+    self._state = np.array([0,0,0,0,0,0])
+    self._episode_ended = False
+    return ts.restart(np.array(self._state, dtype=np.float32))
+
+
+  def _get_state(self, current_observation):
+      self._cur_state = current_observation
+
+  def _desired_state(self, desired_observation):
+      self._desired_state = desired_observation
+
+  def _step(self, action):
+
+    if self._episode_ended:
+      # The last action ended the episode. Ignore the current action and start
+      # a new episode.
+      return self.reset()
+
+    # TODO ACTION (random)
+    action = np.random.rand(6)
+
+    # publish action to simulation
+    error_state = self._desired_state - self._cur_state
+
+    if np.abs(np.mean(error_state)) > 0.1:
+        reward = 0
+        self._episode_ended = True
+
+    if np.abs(np.mean(error_state)) < 0.1 and np.abs(np.mean(error_state)) >0.05:
+        reward = 0.5
+        return ts.termination(np.array(cur_state, dtype=np.float32), reward)
+
+    if np.abs(np.mean(error_state)) < 0.05:
+        reward = 1
+        return ts.termination(np.array(self._cur_state, dtype=np.float32), reward)
+    else:
+        ts.transition(
+          np.array([self._state], dtype=np.int32), reward=0.0, discount=1.0)
+
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+
+    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('/sdu/command/task_space', Float64MultiArray, queue_size=10)
+    listener = tf_data.TransformListener()
+    rate = rospy.Rate(10) # 10hz
+
+    task_space_command_msg = Float64MultiArray()
+
+    while not rospy.is_shutdown():
+        try:
+            (trans, rot) = listener.lookupTransform('/base_link', '/ee_link', rospy.Time(0))
+            print(trans)
+        except (tf_data.LookupException, tf_data.ConnectivityException, tf_data.ExtrapolationException):
+            continue
+
+        task_space_command_msg.data = [0, 0, 0, 0, 0, 0]
+        pub.publish(task_space_command_msg)
+
+        rate.sleep()
+
+
+
