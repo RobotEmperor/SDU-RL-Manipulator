ee2bd58b77fba1897332f35c509536c3fbb34198
robotemperor <kimpd1224@gmail.com>
2020-08-13 14:55:49 +0200

    testing

--- rl-environments/belt_task/envs/belt_task_env.py
+++ rl-environments/belt_task/envs/belt_task_env.py
@@ -2,21 +2,52 @@ import gym
 import numpy as np
 from gym import error, spaces, utils
 
+#ros
+import rospy
+from std_msgs.msg import Bool
+from std_msgs.msg import Float64MultiArray
+#
+error_ee_pose = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
+ee_velocity = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
+contact_force_torque = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
+safety_violation = False
+
+def ErrorEePoseCallBack(data):
+    error_ee_pose = np.array([data.data[0], data.data[1], data.data[2], data.data[3], data.data[4], data.data[5]], dtype=np.float32)
+  #rospy.loginfo(rospy.get_caller_id() + "I ErrorEePoseCallBack %f", data.data[0])
+
+def EeVelocityCallBack(data):
+    ee_velocity = np.array([data.data[0], data.data[1], data.data[2], data.data[3], data.data[4], data.data[5]], dtype=np.float32)
+  #rospy.loginfo(rospy.get_caller_id() + "I EeVelocityCallBack %f", data.data[0])
+
+
+def SafetyViolationCallBack(data):
+  safety_violation = data.data
+  #rospy.loginfo(rospy.get_caller_id() + "I SafetyViolationCallBack %d", data.data)
+
+def  FilteredFtDataCallBack(data):
+  contact_force_torque = np.array([data.data[0], data.data[1], data.data[2], data.data[3], data.data[4], data.data[5]], dtype=np.float32)
+
+
+rospy.init_node('RL_node', anonymous=True)
 
 class BeltTaskEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
+    #init
     self.init_ee_pose = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
-    self.desired_ee_pose = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
 
-    self.max_error_ee_pose = np.array([0.3, 0.3, 0.3, 0.3, 0.3, 0.3], dtype=np.float32)
-    self.max_change_pose = np.array([0.3, 0.3, 0.3, 0.3, 0.3, 0.3], dtype=np.float32)
+    #obs
 
-    self.max_ee_velocity = np.array([0.2, 0.2, 0.2, 0.2, 0.2, 0.2], dtype=np.float32)
-    self.contact_force_torque = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
-    self.max_contact_force_torque = np.array([5, 5, 5, 0.5, 0.5, 0.5], dtype=np.float32)
-    self.ee_pose = self.init_ee_pose
+    self.obs_state = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float32)
+    obs_max_value = np.array([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 10, 10, 10, 1, 1, 1],
+                             dtype=np.float32)  ##error_ee_pose, ee_velocity, force/torque 1*18
+
+    #reward
+    self.max_error_ee_pose = np.array([2, 2, 2, 2, 2, 2], dtype=np.float32)
+    self.max_change_pose = np.array([2, 2, 2, 2, 2, 2], dtype=np.float32)
+    self.max_contact_force_torque = np.array([10, 10, 10, 0.5, 0.5, 0.5], dtype=np.float32)
     self.reward = 0
 
 
@@ -26,16 +57,13 @@ class BeltTaskEnv(gym.Env):
       dtype=np.float32
     )
 
-    high = self.max_error_ee_pose ##error_pose!!
-
     self.observation_space = spaces.Box(
-      low=-high,
-      high=high,
+      low=-obs_max_value,
+      high=obs_max_value,
       dtype=np.float32
     )
 
     self.task_completed = False
-    self.safety_violation = False
     self.w_x = 0
     self.w_a = 0
     self.w_f = 0
@@ -43,13 +71,11 @@ class BeltTaskEnv(gym.Env):
     self.w_r = 0
 
     self.penalty = 0
-    self.pre_defined_reward = 0
-
-    self.pre_ee_pose = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
-
-    self.set_hyperparameters(1, 1, 0, 0, 0)
-
-    self.error_ee_pose = np.array([0, 0, 0, 0, 0, 0], dtype=np.float32)
+    self.set_hyperparameters(1, 1, 0, 0, 1)
+    rospy.Subscriber("/sdu/ur10e/error_ee_pose", Float64MultiArray, ErrorEePoseCallBack)
+    rospy.Subscriber("/sdu/ur10e/ee_velocity", Float64MultiArray, EeVelocityCallBack)
+    rospy.Subscriber("/sdu/ur10e/safety_violation", Bool, SafetyViolationCallBack)
+    rospy.Subscriber("/sdu/ur10e/filtered_force_torque_data", Float64MultiArray, FilteredFtDataCallBack)
 
   def set_hyperparameters(self, w_x, w_a, w_f, w_p, w_r):
     self.w_x = w_x
@@ -59,20 +85,16 @@ class BeltTaskEnv(gym.Env):
     self.w_r = w_r
 
   def norm_l_12(self, z, max_value):
-    max_norm = 0.5 * np.sqrt(6) + np.sqrt(0.1 + 6 ** 2)
+    max_norm = 1
 
     norm = 0.5 * np.sqrt((np.sum(np.square(z)))) + np.sqrt(0.1 + np.sum(np.abs(z))**2)
 
-    #print('norm :',norm)
-    #print('max_norm :', max_norm)
-
     if norm >= max_norm:
-      return -0.1
+      return 0
     elif norm <= 0:
       return 1
     else:
       y = -(1/(max_norm))*norm + 1
-      #y = np.clip(norm, -1, 0)
       return y
 
 
@@ -80,54 +102,34 @@ class BeltTaskEnv(gym.Env):
     #rewards system
     change_pose = action
 
-    self.ee_pose = self.init_ee_pose + change_pose
+    self.modified_ee_pose = change_pose
 
-    self.error_ee_pose = self.desired_ee_pose - self.ee_pose
 
-    ee_velocity = (self.pre_ee_pose - self.ee_pose)/0.002
-
-    check_vel_max = np.greater_equal(ee_velocity, self.max_ee_velocity)
-
-    for i in check_vel_max:
-      if i:
-        self.safety_violation = True
-      else:
-        self.safety_violation = False
-
-    if np.sum(np.abs(self.error_ee_pose)) >= 0 and np.sum(np.abs(self.error_ee_pose)) <= 0.001:
+    if np.sqrt((np.sum(np.square(error_ee_pose)))) <= 0.001:
       self.task_completed = True
       done = True
     else:
+      self.task_completed = False
       done = False
 
     if self.task_completed:
-      reward_k = 200
+      reward_k = 20
+      done = True
     elif self.safety_violation:
       reward_k = -10
+      done = True
     else:
       reward_k = 0
 
-    self.reward = self.w_x * self.norm_l_12(self.error_ee_pose / self.max_error_ee_pose, self.max_error_ee_pose) + self.w_a * self.norm_l_12(change_pose / self.max_change_pose,self.max_change_pose) # + self.w_f * self.norm_l_12(self.contact_force_torque / self.max_contact_force_torque, self.max_contact_force_torque) + self.w_p * 0.1 + self.w_r * reward_k
+    self.reward = self.w_x * self.norm_l_12(error_ee_pose / self.max_error_ee_pose, self.max_error_ee_pose) + self.w_a * self.norm_l_12(change_pose / self.max_change_pose,self.max_change_pose) + self.w_f * self.norm_l_12(contact_force_torque / self.max_contact_force_torque, self.max_contact_force_torque) + self.w_p * self.penalty + self.w_r * reward_k
 
-    self.pre_ee_pose = self.ee_pose
+    #print('self.error_ee_pose :', self.error_ee_pose)
 
-    #print('self.ee_pose :', self.ee_pose)
-    #print('self.action :', action)
-
-    #print('self.action :', action)
-    #print('self.reward :', self.reward)
-    #if self.reward > 0.95:
-    #print('self.error_norm :', np.sqrt(np.sum(np.square(self.error_ee_pose))))
-
-    return self._get_obs(), self.reward, done, {}
+    return self._get_obs(), self.reward, False, {}
 
   def reset(self):
-    #self.desired_ee_pose = np.random.rand(6)
-    #self.desired_ee_pose = np.clip(self.desired_ee_pose, -self.max_error_ee_pose/2, self.max_error_ee_pose/2)
-    self.init_ee_pose = np.random.rand(6)
-    self.init_ee_pose = np.clip(self.init_ee_pose, -self.max_error_ee_pose/2, self.max_error_ee_pose/2)
-    self.error_ee_pose = self.desired_ee_pose - self.init_ee_pose
     return self._get_obs()
 
   def _get_obs(self):
-    return self.error_ee_pose
+    return self.obs_state
+
